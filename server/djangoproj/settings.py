cf75de288236a21102b374de12865bd528395d49
BankNatawat <natawatbank@gmail.com>
2026-02-22 20:54:50 +0700

    add django environment into local machine

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',
-    'https://natawatbank-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = ['https://natawatbank-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
+CSRF_TRUSTED_ORIGINS = []
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
